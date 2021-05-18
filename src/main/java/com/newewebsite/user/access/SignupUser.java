3be3bd57e462df9ff3c70c5ea0ad86cc05aec056
Rakshith Shetty <rshetty89@gmail.com>
2021-05-18 20:14:13 +0530

    some comments

---
+++ src/main/java/com/newewebsite/user/access/SignupUser.java
@@ -0,0 +1,63 @@
+package com.newewebsite.user.access;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.newewebsite.user.UserDetails;
+
+/**
+ * Servlet implementation class SignupUser
+ */
+public class SignupUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Configuration configuration = new Configuration().configure();
+		configuration.addAnnotatedClass(com.newewebsite.user.UserDetails.class);
+		StandardServiceRegistryBuilder builder 
+				= new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory(builder.build());
+		
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		UserDetails user = (UserDetails) request;
+		session.save(user);
+		transaction.commit();
+		System.out.println("Recodrd inserted");
+		session.close();
+		
+		     		
+		
+	}
+
+}
